d3e7920d4f5776eb07467607985765f9c72b1774
karimahamd98 <99287573+karimahamd98@users.noreply.github.com>
2022-02-12 15:32:19 +0200

    Update covidmaps-st.py

--- covidmaps-st.py
+++ covidmaps-st.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly
-from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
-from plotly.graph_objs import *
-init_notebook_mode()
-import plotly.express as px
+
 from PIL import Image
 image = Image.open('banner-covid-19-1280x549.webp')
 st.image(image, width= 600)
